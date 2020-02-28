9b5da3f7780856737534e5fadae558231d32e738
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-28 03:50:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 
 class Song
 
@@ -17,8 +17,6 @@ class Song
 
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
-    #binding.pry
-   
     song = self.new(song_name)
    song.artist_name = file.split(" - ")[0]
    song
